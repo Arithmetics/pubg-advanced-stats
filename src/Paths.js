9b80a58defdf3f496c5d78382eaacffb368ba29e
Brock Tillotson <brock.tillotson@opusteam.com>
2018-05-19 13:42:22 -0700

    pushing to prod

--- src/Paths.js
+++ src/Paths.js
@@ -106,29 +106,126 @@ export default class Paths extends Component {
 
   render(){
     console.log("TELEMETRY", this.props.telemetry);
-    const colorArray =
-        ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black",
-        "BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse",
-        "Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan",
-        "DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta",
-        "DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen",
-        "DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink",
-        "DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen",
-        "Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow",
-        "HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush",
-        "LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow",
-        "LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen",
-        "LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow",
-        "Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue",
-        "MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen",
-        "MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin",
-        "NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid",
-        "PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff",
-        "Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue",
-        "SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver",
-        "SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue",
-        "Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"
-      ];
+
+    const colorArray = [
+  "#FD3A4A",
+  "#FFF700",
+  "#299617",
+  "#A7F432",
+  "#2243B6",
+  "#5DADEC",
+  "#5946B2",
+  "#9C51B6",
+  "#FA5B3D",
+  "#A83731",
+  "#AF6E4D",
+  "#FF5470",
+  "#FFDB00",
+  "#FB4D46",
+  "#FF7A00",
+  "#FDFF00",
+  "#FFAA1D",
+  "#87FF2A",
+  "#0048BA",
+  "#FF3855",
+  "#FF00CC",
+  "Aqua",
+  "Aquamarine",
+  "Blue",
+  "BlueViolet",
+  "Brown",
+  "CadetBlue",
+  "Chartreuse",
+  "Chocolate",
+  "Coral",
+  "CornflowerBlue",
+  "Crimson",
+  "Cyan",
+  "DarkBlue",
+  "DarkCyan",
+  "DarkGreen",
+  "DarkKhaki",
+  "DarkMagenta",
+  "DarkOliveGreen",
+  "DarkOrchid",
+  "DarkRed",
+  "#CC00FF",
+  "DarkTurquoise",
+  "DarkViolet",
+  "DeepPink",
+  "DimGray",
+  "DimGrey",
+  "DodgerBlue",
+  "FireBrick",
+  "ForestGreen",
+  "Fuchsia",
+  "Gold",
+  "Gray",
+  "Green",
+  "GreenYellow",
+  "HotPink",
+  "IndianRed",
+  "Indigo",
+  "Khaki",
+  "Lavender",
+  "LawnGreen",
+  "LightBlue",
+  "LightCoral",
+  "LightCyan",
+  "LightGray",
+  "LightGrey",
+  "LightGreen",
+  "LightPink",
+  "LightSalmon",
+  "LightSeaGreen",
+  "LightSkyBlue",
+  "Lime",
+  "LimeGreen",
+  "Magenta",
+  "Maroon",
+  "MediumBlue",
+  "MediumOrchid",
+  "MediumPurple",
+  "MediumSeaGreen",
+  "MediumSlateBlue",
+  "MediumSpringGreen",
+  "MediumTurquoise",
+  "MediumVioletRed",
+  "MidnightBlue",
+  "Olive",
+  "OliveDrab",
+  "Orange",
+  "OrangeRed",
+  "Orchid",
+  "PaleGoldenRod",
+  "PaleGreen",
+  "PaleTurquoise",
+  "PaleVioletRed",
+  "Peru",
+  "Pink",
+  "Plum",
+  "Purple",
+  "Red",
+  "RoyalBlue",
+  "SaddleBrown",
+  "Salmon",
+  "SandyBrown",
+  "SeaGreen",
+  "Sienna",
+  "Silver",
+  "SkyBlue",
+  "SlateBlue",
+  "SpringGreen",
+  "SteelBlue",
+  "Tan",
+  "Teal",
+  "Thistle",
+  "Tomato",
+  "Turquoise",
+  "Violet",
+  "Yellow",
+  "YellowGreen"
+];
 
     const graphStyle = {}
 
@@ -262,7 +359,8 @@ export default class Paths extends Component {
         onChange={this.handleLowChange}
         onChangeComplete={this.handleChangeComplete}
       />
-    <p className='label'>Max Time</p>
+
+      <p className='label'>Min Time</p>
       <Slider
         min={0}
         max={2000}
@@ -272,19 +370,21 @@ export default class Paths extends Component {
         onChangeComplete={this.handleChangeComplete}
       />
     </div>
-    <button
-      className="pubg-button"
-      onClick={this.handleTeamViewToggle}
-      style={this.state.teamView ? {color: "black", backgroundColor:"#f8a427"} : {color: "#f8a427", backgroundColor:"black"}}
-    >TeamView</button>
-    <button
-      className="pubg-button"
-      onClick={this.handlePlayerViewToggle}
-      style={this.state.playerView ? {color: "black", backgroundColor:"#f8a427"} : {color: "#f8a427", backgroundColor:"black"}} >PlayerView</button>
-    <button
-      className="pubg-button"
-      onClick={this.handleWinnersViewToggle}
-      style={this.state.winnersView ? {color: "black", backgroundColor:"#f8a427"} : {color: "#f8a427", backgroundColor:"black"}} >WinnerView</button>
+    <div class="button-zone">
+      <button
+        className="pubg-button"
+        onClick={this.handleTeamViewToggle}
+        style={this.state.teamView ? {color: "black", backgroundColor:"#f8a427"} : {color: "#f8a427", backgroundColor:"black"}}
+      >TeamView</button>
+      <button
+        className="pubg-button"
+        onClick={this.handlePlayerViewToggle}
+        style={this.state.playerView ? {color: "black", backgroundColor:"#f8a427"} : {color: "#f8a427", backgroundColor:"black"}} >PlayerView</button>
+      <button
+        className="pubg-button"
+        onClick={this.handleWinnersViewToggle}
+        style={this.state.winnersView ? {color: "black", backgroundColor:"#f8a427"} : {color: "#f8a427", backgroundColor:"black"}} >WinnerView</button>
+    </div>
   </div>
     )
   }
